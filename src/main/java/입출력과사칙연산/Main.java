1422ba93693ec2f27d06a353420488797136a8bf
yeojin822 <xlzt2002@gmail.com>
2022-06-17 14:05:14 +0900

    feat : 입출력과 사칙연산 추가

--- src/main/java/입출력과사칙연산/Main.java
+++ src/main/java/입출력과사칙연산/Main.java
@@ -1,11 +1,174 @@
 package 입출력과사칙연산;
 
-public class Main {
+/**
+ * Hello World
+ */
+// public class Main {
+//    public static void main(String[] args) {
+//
+//        System.out.println("Hello World!");
+//    }
+//}
 
-   /**
-    * Hello World
-    */
+/**
+ * We love kriii
+ */
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        System.out.println("강한친구 대한육군");
+//        System.out.println("강한친구 대한육군");
+//    }
+//}
+
+/**
+ * 고양이
+ */
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("\\    /\\");
+//        System.out.println(" )  ( ')");
+//        System.out.println("(  /  )");
+//        System.out.println(" \\(__)|");
+//    }
+//}
+
+/**
+ * 개
+ */
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("|\\_/|\n" +
+//                "|q p|   /}\n" +
+//                "( 0 )\"\"\"\\\n" +
+//                "|\"^\"`    |\n" +
+//                "||_/=\\\\__|");
+//    }
+//}
+
+/**
+ * A+B
+ */
+// import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        System.out.println(a+b);
+//    }
+//}
+
+/**
+ * A-B
+ */
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        System.out.println(a-b);
+//    }
+//}
+
+/**
+ * A*B
+ */
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        System.out.println(a*b);
+//    }
+//}
+
+/**
+ * 사칙연산
+ */
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int A = scanner.nextInt();
+//        int B = scanner.nextInt();
+//        System.out.println(A+B);
+//        System.out.println(A-B);
+//        System.out.println(A*B);
+//        System.out.println(A/B);
+//        System.out.println(A%B);
+//    }
+//}
+
+/**
+ * ??!
+ */
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        String id = scanner.nextLine();
+//        System.out.println(id+"??!");
+//    }
+//}
+
+/**
+ * 1998년생인 내가 태국에서는 2541년생?!
+ */
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int year = scanner.nextInt();
+//        System.out.println(year-543);
+//    }
+//}
+
+/**
+ * 나머지
+ */
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int A = scanner.nextInt();
+//        int B = scanner.nextInt();
+//        int C = scanner.nextInt();
+//        System.out.println((A+B)%C);
+//        System.out.println(((A%C) + (B%C))%C);
+//        System.out.println((A*B)%C);
+//        System.out.println(((A%C) * (B%C))%C);
+//    }
+//}
+
+/**
+ * 곱셈
+ */
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int A = scanner.nextInt();
+//        int B = scanner.nextInt();
+//        System.out.println(A * (B %10));
+//        System.out.println(A * ((B/10) %10));
+//        System.out.println(A * (((B/10)/10) %10));
+//        System.out.println(A * B);
+//    }
+//}
+
+/**
+ * 새싹
+ */
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
     }
 }
