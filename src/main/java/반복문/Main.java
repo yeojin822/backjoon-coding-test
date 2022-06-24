fc1529b7a19ce6fa22bfac90884a7bb799cd90a9
yeojin822 <xlzt2002@gmail.com>
2022-06-24 12:43:15 +0900

    feat : 반복문 추가

---
+++ src/main/java/반복문/Main.java
@@ -0,0 +1,16 @@
+package 반복문;
+
+/**
+ * 구구단
+ */
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int dan = scanner.nextInt();
+//        for (int i = 1; i <= 9; i++) {
+//            System.out.println( dan + " * " +i +" = " + dan*i);
+//        }
+//    }
+//}
+
