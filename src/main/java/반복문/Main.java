a45e3e20191bfd5f00928b478e86c6b8775098f0
yeojin822 <xlzt2002@gmail.com>
2022-06-26 18:43:59 +0900

    feat : 조건문 추가

--- src/main/java/반복문/Main.java
+++ src/main/java/반복문/Main.java
@@ -14,3 +14,26 @@ package 반복문;
 //    }
 //}
 
+/**
+ * 	A+B - 3
+ */
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        int[] arrayA = new int[T];
+        int[] arrayB = new int[T];
+        scanner.nextLine();
+        for (int i = 0; i < T; i++) {
+            String num = scanner.nextLine();
+            arrayA[i] = Integer.parseInt( num.split(" ")[0]);
+            arrayB[i] = Integer.parseInt( num.split(" ")[1]);
+        }
+
+        for (int i = 0; i < T; i++) {
+            System.out.println(arrayA[i] + arrayB[i]);
+        }
+
+    }
+}
