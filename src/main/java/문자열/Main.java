e33450c7e2fb2ad9f15fbf29d025dde11eb8fe49
yeojin822 <xlzt2002@gmail.com>
2022-06-30 14:55:40 +0900

    feat : 문자열 추가

---
+++ src/main/java/문자열/Main.java
@@ -0,0 +1,57 @@
+package 문자열;
+
+/**
+ * 아스키 코드
+ */
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        char c = sc.nextLine().charAt(0);
+//        System.out.println((int)c);
+//    }
+//}
+
+/**
+ * 숫자의 합
+ */
+//import java.util.Arrays;
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int N = Integer.parseInt(sc.nextLine());
+//        int[] array = new int[N];
+//        String num = sc.nextLine();
+//        for (int i = 0; i < N; i++) {
+//            array[i] = Integer.parseInt(String.valueOf(num.charAt(i)));
+//        }
+//        System.out.println(Arrays.stream(array).sum());
+//    }
+//}
+
+
+/**
+ * 알파벳 찾기
+ */
+import java.util.Arrays;
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int[] array  = new int[26];
+//        String word = sc.nextLine();
+//
+//        for (int i = 0; i < word.length(); i++) {
+//            for (int j = 97; j < 123 ; j++) {
+//                if((int)word.charAt(i) == j){
+//                    array[i] +=1;
+//                }else{
+//                    array[i] = -1;
+//                }
+//            }
+//        }
+//    }
+//}
+
+
