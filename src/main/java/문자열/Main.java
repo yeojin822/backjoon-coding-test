b9c9889cd0048afb0b7c75caa62a54abc95880c8
yeojin822 <xlzt2002@gmail.com>
2022-07-08 23:13:39 +0900

    fix : 문자열 수정

--- src/main/java/문자열/Main.java
+++ src/main/java/문자열/Main.java
@@ -38,30 +38,100 @@ package 문자열;
 /**
  * 알파벳 찾기
  */
+//import java.util.Arrays;
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int[] array = new int[26];
+//        for (int i = 0; i <array.length ; i++) {
+//            array[i] = -1;
+//        }
+//
+//        String word = sc.nextLine();
+//        for (int i = 0; i < word.length(); i++) {
+//             for (int j = 97; j < 123; j++) {
+//                    if (array[j - 97] == -1 && (int)word.charAt(i) == j) {
+//                        array[j - 97] = i;
+//                        break;
+//                    }
+//            }
+//        }
+//        String test = Arrays.toString(array);
+//        test = test.replace("[","").replace("]","").replace("," , "");
+//        System.out.println(test);
+//    }
+//}
 
-import java.util.Arrays;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] array = new int[26];
-        for (int i = 0; i <array.length ; i++) {
-            array[i] = -1;
-        }
+/**
+ * 문자열 반복
+ */
+//import java.io.*;
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int T = Integer.parseInt(br.readLine());
+//        int[] repeat = new int[T];
+//        String[] array = new String[T];
+//        String[] newArray = new String[T];
+//
+//        for (int i = 0; i < T; i++) {
+//            String[] temp = br.readLine().split(" ");
+//            repeat[i] = Integer.parseInt(temp[0]);
+//            array[i] = temp[1];
+//        }
+//
+//        for (int i = 0; i <array.length ; i++) {
+//            newArray[i] = "";
+//            for (int j = 0; j < array[i].length(); j++) {
+//                newArray[i] += String.valueOf(array[i].charAt(j)).repeat(repeat[i]);
+//            }
+//        }
+//
+//        for (int i = 0; i < newArray.length; i++) {
+//            System.out.println(newArray[i]);
+//        }
+//    }
+//}
 
-        String word = sc.nextLine();
-        for (int i = 0; i < word.length(); i++) {
-             for (int j = 97; j < 123; j++) {
-                    if (array[j - 97] == -1 && (int)word.charAt(i) == j) {
-                        array[j - 97] = i;
-                        break;
-                    }
-            }
-        }
-        String test = Arrays.toString(array);
-        test = test.replace("[","").replace("]","").replace("," , "");
-        System.out.println(test);
-    }
-}
+///**
+// * 단어 공부
+// */
+//import java.io.*;
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String word = br.readLine().toUpperCase();
+//        String answer = "";
+//        int max = 0;
+//        char ch = 0;
+//        boolean dist = false;
+//
+//        for (int i = 0; i < word.length(); i++) {
+//            if(word.indexOf(word.charAt(i)) == i) {
+//                int cnt = 0;
+//                for (int j = 0; j < word.length(); j++) {
+//                    if(word.charAt(i) == word.charAt(j)){
+//                        cnt++;
+//                    }
+//                }
+//                if(cnt > max){
+//                    max = cnt;
+//                    ch = word.charAt(i);
+//                }else if(cnt == max){
+//                    dist = true;
+//                    break;
+//                }
+//            }
+//        }
+//
+//        if(dist){
+//            System.out.println("?");
+//        }else{
+//            System.out.println(ch);
+//        }
+//    }
+//}
+
 
 
