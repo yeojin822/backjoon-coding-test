f77d24091d5ed22a55f9c7df3d1d9e31583a1fbe
yeojin822 <xlzt2002@gmail.com>
2022-08-16 12:14:30 +0900

    feat : 기본수학1 벌집 추가

--- src/main/java/기본수학1/Main.java
+++ src/main/java/기본수학1/Main.java
@@ -4,27 +4,49 @@ package 기본수학1;
  * 손익분기점
  */
 
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//public class Main {
+//    /*
+//    * (n * 상품가격 ) = 불변비용 + (가변비용 * n)
+//    * (n * 상품가격 ) - (가변비용 * n) = 불변비용
+//    * n(상품가격 - 가변비용) = 불변비용
+//    * n = 불변이용/(상품가격 - 가변비용)
+//    * 손익분기점 => n = 불변이용/(상품가격 - 가변비용) +1
+//    * */
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String[] array = br.readLine().split(" ");
+//
+//        if (Integer.parseInt(array[2]) <= Integer.parseInt(array[1])) {
+//            System.out.println("-1");
+//        }
+//        else {
+//            System.out.println((Integer.parseInt(array[0])/(Integer.parseInt(array[2])-Integer.parseInt(array[1])))+1);
+//        }
+//
+//    }
+//}
+
+/**
+ * 벌집
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 public class Main {
-    /*
-    * (n * 상품가격 ) = 불변비용 + (가변비용 * n)
-    * (n * 상품가격 ) - (가변비용 * n) = 불변비용
-    * n(상품가격 - 가변비용) = 불변비용
-    * n = 불변이용/(상품가격 - 가변비용)
-    * 손익분기점 => n = 불변이용/(상품가격 - 가변비용) +1
-    * */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] array = br.readLine().split(" ");
+        int N = Integer.parseInt(br.readLine());
+        int start = 1;
+        int totalCnt = 1;
 
-        if (Integer.parseInt(array[2]) <= Integer.parseInt(array[1])) {
-            System.out.println("-1");
+        while (N > totalCnt){
+            totalCnt += (6*start);
+            start++;
         }
-        else {
-            System.out.println((Integer.parseInt(array[0])/(Integer.parseInt(array[2])-Integer.parseInt(array[1])))+1);
-        }
-
+        System.out.println(start);
     }
 }
